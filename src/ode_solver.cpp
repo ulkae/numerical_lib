2d8a357a81b10780a7888a9964a2dd42cf706502
olap732 <aprzewoznik@student.agh.edu.pl>
2025-06-13 10:52:21 +0200

    Update ode_solver.cpp

--- src/ode_solver.cpp
+++ src/ode_solver.cpp
@@ -1,5 +1,5 @@
 #include "ode_solver.h"
-
+//rozwiazuje rownanie rozniczkowe y'(t) = f(t,y) metoda Runge-Kutta 4. rzedu
 vector<double> rungeKutta4(double (*f)(double, double), double y0, double t0, double t_end, int steps) {
     vector<double> result;
     double h = (t_end - t0) / steps;
@@ -8,7 +8,7 @@ vector<double> rungeKutta4(double (*f)(double, double), double y0, double t0, do
 
     for (int i = 0; i <= steps; ++i) {
         result.push_back(y);
-
+//obliczanie wspolczynnikow k1, k2, k3, k4 metody RK4
         double k1 = h * f(t, y);
         double k2 = h * f(t + h / 2, y + k1 / 2);
         double k3 = h * f(t + h / 2, y + k2 / 2);
