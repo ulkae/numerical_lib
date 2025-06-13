eca4c315fc89ff9854dfee251d1b07f1b2e7f504
olap732 <aprzewoznik@student.agh.edu.pl>
2025-06-13 10:23:11 +0200

    Update linear_systems.h

--- include/linear_systems.h
+++ include/linear_systems.h
@@ -2,6 +2,9 @@
   #include <vector>
  using namespace std;
 
+//rozkładanie macierzy A na macierze L (dolna trójkątna) i U (górna trójkątna)
  void luDecomposition(const vector<vector<double>>& A, vector<vector<double>>& L, vector<vector<double>>& U);
+//rozwiązuje Lz = b (podstawianie w przód)
  vector<double> forwardSubstitution(const vector<vector<double>>& L, const vector<double>& b);
+//rozwiązuje Ux = z (podstawianie wstecz)
  vector<double> backSubstitution(const vector<vector<double>>& U, const vector<double>& z);
