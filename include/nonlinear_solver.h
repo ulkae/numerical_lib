f8f97c8282f6e7276280994e853acc0fa1f4b4c3
olap732 <aprzewoznik@student.agh.edu.pl>
2025-06-13 10:24:40 +0200

    Update nonlinear_solver.h

--- include/nonlinear_solver.h
+++ include/nonlinear_solver.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <functional>
 using namespace std;
+//znajdowanie pierwiastka równania f(x)=0 metodą Newtona-Raphsona
 double newtonAnalytical(function<double(double)> f, function<double(double)> df, double x0, double epsilon = 1e-8, int max_iter = 100);
