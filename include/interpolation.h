05dd4df37497125fe784f9622689c99f91c5c6a9
olap732 <aprzewoznik@student.agh.edu.pl>
2025-06-13 10:16:23 +0200

    Update interpolation.h

--- include/interpolation.h
+++ include/interpolation.h
@@ -1,6 +1,8 @@
 #pragma once
    #include <vector>
       using namespace std;
-   
+
+   //obliczanie współczynników interpolacji Newtona
       vector<double> computeNewtonCoefficients(const vector<double>& x, const vector<double>& y);
+  //obliczanie interpolowanej wartości w punkcie x_val
       double newtonInterpolation(double x_val, const vector<double>& x, const vector<double>& coefficients);
