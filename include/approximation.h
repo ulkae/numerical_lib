82b689f0c151409755428b820c2f381de707c764
olap732 <aprzewoznik@student.agh.edu.pl>
2025-06-13 10:13:58 +0200

    Update approximation.h

--- include/approximation.h
+++ include/approximation.h
@@ -1,6 +1,8 @@
 #pragma once
     #include <vector>
      using namespace std;
-   
+
+   //obliczanie współczynników wielomianu
       vector<double> computeLeastSquares(const vector<double>& x, const vector<double>& y, int degree);
+  //obliczanie wartości wielomianu w danym punkcie
       double evaluatePolynomial(double x, const vector<double>& coeffs);
