900acfb1daad8596350a21b4eec0970abf1cc5c1
olap732 <aprzewoznik@student.agh.edu.pl>
2025-06-13 10:14:51 +0200

    Update integration.h

--- include/integration.h
+++ include/integration.h
@@ -1,5 +1,6 @@
 #pragma once
       #include <functional>
       using namespace std;
-   
+
+//obliczanie całki funkcji na przedziale [a,b] metodą Gaussa-Legendre'a
       double gaussLegendre(function<double(double)> f, double a, double b, int n);
