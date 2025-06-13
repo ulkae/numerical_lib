5da215798aa869379021962f1d98328718c351de
olap732 <aprzewoznik@student.agh.edu.pl>
2025-06-13 10:25:23 +0200

    Update ode_solver.h

--- include/ode_solver.h
+++ include/ode_solver.h
@@ -2,4 +2,5 @@
 #include <vector>
 using namespace std;
 
+//rozwiązuje równanie y'(t) = f(t,y) metodą Runge-Kutta 4. rzędu
 vector<double> rungeKutta4(double (*f)(double, double), double y0, double t0, double t_end, int steps);
